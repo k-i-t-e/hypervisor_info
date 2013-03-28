ec0c36c81a9a1e017bd4d0a139f3a68279ce8d2e
k-i-t-e <darkmik.kite@gmail.com>
2013-03-28 21:18:31 +0400

    first commit

---
+++ hypervisor_info/test.py
@@ -0,0 +1,21 @@
+'''
+Created on Mar 28, 2013
+
+@author: kite
+'''
+import libvirt
+import sys
+
+conn = libvirt.openReadOnly("qemu:///system")
+if conn == None:
+    print 'Failed to open connection to hypervisor'
+    sys.exit(1)
+
+try:
+    dom0 = conn.lookupByName("Domain-0")
+except:
+    print 'Failed to find the main domain'
+    sys.exit(1)
+
+print "Domain 0: id %d running %s" % (dom0.ID(), dom0.OSType())
+print dom0.info()
