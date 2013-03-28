fdd25e3a68ebb11c748fb141d42200d1cd78da71
k-i-t-e <darkmik.kite@gmail.com>
2013-03-28 21:28:39 +0400

    changed

--- hypervisor_info/test.py
+++ hypervisor_info/test.py
@@ -11,11 +11,15 @@ if conn == None:
     print 'Failed to open connection to hypervisor'
     sys.exit(1)
 
-try:
-    dom0 = conn.lookupByName("Domain-0")
-except:
-    print 'Failed to find the main domain'
-    sys.exit(1)
+domains = conn.listDomainsID()
 
-print "Domain 0: id %d running %s" % (dom0.ID(), dom0.OSType())
-print dom0.info()
+print domains
+
+#try:
+#    dom0 = conn.lookupByName("Domain-0")
+#except:
+#    print 'Failed to find the main domain'
+#    sys.exit(1)
+
+#print "Domain 0: id %d running %s" % (dom0.ID(), dom0.OSType())
+#print dom0.info()
