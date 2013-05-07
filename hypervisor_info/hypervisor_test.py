5f0b9ca4811327b2cb1cc08d19955c27a9afe89e
k-i-t-e <darkmik.kite@gmail.com>
2013-05-07 13:58:49 +0400

    with appropriate test

--- hypervisor_info/hypervisor_test.py
+++ hypervisor_info/hypervisor_test.py
@@ -7,25 +7,32 @@ Created on Apr 12, 2013
 import hypervisor_info
 from algorithm import ScheduldingAlgorithm
 
-npc11 = hypervisor_info.HypervisorInfo("qemu:///system", 'npc11')
-npc10 = hypervisor_info.HypervisorInfo("qemu+ssh://root@195.208.117.184/system", 'npc10')
+URIs = ["qemu:///system", "qemu+ssh://root@195.208.117.184/system"]
+hostnames = ['npc11', 'npc10']
 
-print npc11.getDomainStats(10)
-npc11.showVMs()
+#npc11 = hypervisor_info.HypervisorInfo("qemu:///system", 'npc11')
+#npc10 = hypervisor_info.HypervisorInfo("qemu+ssh://root@195.208.117.184/system", 'npc10')
 
-print npc10.getDomainStats(10)
-npc10.showVMs()
+cloud = hypervisor_info.HypervisorInfo(URIs, hostnames)
 
-test = ScheduldingAlgorithm()
+print cloud.getDomainStats(10)
 
-test.VMs = []
-test.VMs.extend(npc10.host.assigned_vms)
-test.VMs.extend(npc11.host.assigned_vms)
-test.hosts = [npc10.host, npc11.host]
-
-test.show()
-
-i = test.simulated_annealing_abstract(test.root_mean_sqr)
-
-test.show_hosts()
-print 'made '+str(i)+' iterations'
+#print npc11.getDomainStats(10)
+#npc11.showVMs()
+#
+#print npc10.getDomainStats(10)
+#npc10.showVMs()
+#
+#test = ScheduldingAlgorithm()
+#
+#test.VMs = []
+#test.VMs.extend(npc10.host.assigned_vms)
+#test.VMs.extend(npc11.host.assigned_vms)
+#test.hosts = [npc10.host, npc11.host]
+#
+#test.show()
+#
+#i = test.simulated_annealing_abstract(test.root_mean_sqr)
+#
+#test.show_hosts()
+#print 'made '+str(i)+' iterations'
